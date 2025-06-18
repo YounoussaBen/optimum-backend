10898023b799a6c434c8be03a669f347c286e402
YounoussaBen <younoussaabdourhaman@gmail.com>
2025-06-18 22:36:50 +0000

    setup: fix prod config

--- startup.sh
+++ startup.sh
@@ -19,7 +19,7 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    User.objects.create_superuser('admin', 'test@admin.com', '1234')
     print('Superuser created')
 else:
     print('Superuser already exists')
